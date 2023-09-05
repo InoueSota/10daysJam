235ed2980049e16b42f111d008bb07f92515c6f2
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2023-09-05 17:53:17 +0900

    a

--- Assets/Script/ChildManager.cs
+++ Assets/Script/ChildManager.cs
@@ -16,6 +16,8 @@ public class ChildManager : MonoBehaviour
     float diff = 0f;
     Vector2 diffPosition = Vector2.zero;
 
+   
+
     private Rigidbody2D rb;
 
     private int playerDirection = 1;
@@ -27,16 +29,23 @@ public class ChildManager : MonoBehaviour
         Follow,//親についていく
         Dash,//突撃
         Stack,//かさねる
+        StackAttack,
         Stay//そのばたいき
     }
 
     [SerializeField] private MoveType moveType = MoveType.Follow;
 
-
+    Vector3 vec = Vector3.zero;
 
 
     private int OrderDirection = 0;
 
+    private static int stackCount = 0;
+    private int stackIndex = 0;
+
+    public bool isPileUpped = false;
+
+    private Vector3 stackPos = Vector3.zero;
 
     void Start()
     {
@@ -49,18 +58,73 @@ public class ChildManager : MonoBehaviour
 
     void Update()
     {
-        if (playerManager.orderRight == true)
+        if (moveType == MoveType.Stack)
         {
-            SetMove(1);
-            OrderDirection = 1;
+            if (playerManager.orderRight == true)
+            {
+
+                OrderDirection = 1;
+
+
+                vec.x = dashSpeed * OrderDirection;
+                vec.y = dashSpeed;
+                stackCount = 0;
+                isPileUpped = false;
+                SetMove(3);
+            }
+
+            if (playerManager.orderLeft == true)
+            {
+                OrderDirection = -1;
+
+                vec.x = dashSpeed * OrderDirection;
+                vec.y = dashSpeed;
+                isPileUpped = false;
+                stackCount = 0;
+                SetMove(3);
+            }
+        }
+        else
+        {
+
+            if (playerManager.orderRight == true)
+            {
+
+                OrderDirection = 1;
+                SetMove(1);
+            }
+
+            if (playerManager.orderLeft == true)
+            {
+
+                OrderDirection = -1;
+                SetMove(1);
+
+            }
         }
 
-        if (playerManager.orderLeft == true)
+        if (playerManager.orderPileUp == true)
         {
-            SetMove(1);
-            OrderDirection = -1;
+            stackIndex = stackCount;
+            stackPos.x = playerManager.transform.position.x;
+            stackPos.y = (playerManager.transform.position.y + playerManager.transform.localScale.y * 0.5f) + transform.localScale.y * 0.5f;
+            isPileUpped = true;
+            SetMove(2);
+            stackCount++;
         }
 
+        if(playerManager.orderDown == true)
+        {
+
+            Vector3 vec = Vector3.zero;
+
+            vec.x = 0.0f;
+            vec.y = 0.0f;
+
+            isPileUpped = false;
+            stackCount = 0;
+            SetMove(0);
+        }
 
         Move();
     }
@@ -84,52 +148,74 @@ public class ChildManager : MonoBehaviour
                 break;
             case MoveType.Stack:
 
-               break;
-            //case MoveType.Stay:
+                MoveStack();
 
-            //    break;
+                break;
+
+            case MoveType.StackAttack:
+
+                MoveStackAttack();
+
+                break;
+                //case MoveType.Stay:
+
+                //    break;
         }
     }
 
-    //動きをセットする、1度だけの処理もここでする
+    //動きをセットする
     public void SetMove(int type)
     {
         moveType = (MoveType)Enum.ToObject(typeof(MoveType), type);
 
-        //switch (moveType)
-        //{
-        //    case MoveType.Follow:
-        //        //ナシ
-        //        break;
-        //    case MoveType.Dash:
-
-        //        OrderDirection = (int)playerManager.GetDirection();
-
-        //        break;
-        //    case MoveType.Stack:
-
-        //        break;
-        //}
+       
     }
 
     //フォロー時の動き
     void MoveFollow()
     {
         GetPlayerDiffPosition();
-
+        
         transform.position += new Vector3(diffPosition.x - transform.position.x, 0f, 0f) * (followValue * Time.deltaTime);
+
+        vec.y -= 3.0f * Time.deltaTime * 9.81f;
+        rb.velocity = vec;
     }
 
     //ダッシュ時の動き
     void MoveDash()
     {
 
-        Vector3 vec = Vector3.zero;
+        vec = Vector3.zero;
 
         vec.x = dashSpeed;
 
         
         vec.x *= OrderDirection;
+        rb.velocity = vec;
+    }
+
+    void MoveStack()
+    {
+
+        Vector3 pos = stackPos;
+
+        pos.x = playerManager.transform.position.x;
+        pos.y = stackPos.y + stackIndex * transform.localScale.y;
+
+        transform.position = pos;
+
+    }
+
+    void MoveStackAttack()
+    {
+
+        
+
+        vec.y -= 3.0f * Time.deltaTime * 9.81f;
+        vec.x -= 3.0f * Time.deltaTime ;
+
+        
         rb.velocity = vec;
     }
 
@@ -148,4 +234,6 @@ public class ChildManager : MonoBehaviour
 
         diffPosition = new(player.transform.position.x + diff, player.transform.position.y);
     }
+
+   
 }
