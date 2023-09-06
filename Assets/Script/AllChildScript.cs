7d0707eff736b660151f001e449702da6eb8fd1e
KanaiMint <115054962+KanaiMint@users.noreply.github.com>
2023-09-06 15:33:30 +0900

    a

--- Assets/Script/AllChildScript.cs
+++ Assets/Script/AllChildScript.cs
@@ -6,32 +6,46 @@ public class AllChildScript : MonoBehaviour
 {
    public int childCount;
     Transform parentTransform;
+    public int PineNum;
     // Start is called before the first frame update
     void Start()
     {
-        // 親オブジェクトのTransformコンポーネントを取得
-        parentTransform = transform;
-
-        // 子オブジェクトの数を数える
-        childCount = parentTransform.childCount;
-
+        //// 親オブジェクトのTransformコンポーネントを取得
+        //parentTransform = transform;
+        //// 子オブジェクトの数を数える
+        //childCount = parentTransform.childCount;
+        // 最初に一度だけ子オブジェクトを数える
+        CountPineNum();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 親オブジェクトのTransformコンポーネントを取得
-        parentTransform = transform;
-
-        // 子オブジェクトの数を数える
-        childCount = parentTransform.childCount;
-        // 結果をコンソールに出力
-        Debug.Log("子オブジェクトの数: " + childCount);
-
-        for(int i = 0; i < childCount; i++)
-        {
-           
-        }
+        CountPineNum();
     }
+
+    void CountPineNum()
+    {
+        PineNum = 0;
+
+        // 子オブジェクトをループして条件をチェック
+        foreach (Transform child in transform)
+        {
+            ChildManager childManager = child.GetComponent<ChildManager>();
+            if (childManager != null && childManager.isPileUpped)
+            {
+                PineNum++;
+            }
+        }
+
+        // 結果をコンソールに出力
+        Debug.Log("PineNum: " + PineNum);
+    }
+
+
+
+
+
+
 }
